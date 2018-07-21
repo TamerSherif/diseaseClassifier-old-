0534ac8d2d7e456cb29489fc516182b09b61cb45
TamerSherif <tamer.m.sherif@hotmail.com>
2018-07-21 19:59:42 -0400

    created upload component

---
+++ frontend/src/components/upload.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+class  extends Component {
+    state = {  
+        image = ''
+    };
+
+    render() { 
+        return (  
+            //upload button
+            //when upload button is clicked upload popup
+            //save this image as a state and let it trigger the backend
+            
+
+        );
+    }
+}
+ 
+export default ;
