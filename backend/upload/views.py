0cbb644a08cddb281c10fdd13d32f338cf4f9076
TamerSherif <tamer.m.sherif@hotmail.com>
2018-07-27 23:48:01 -0400

    connected db to uploads

--- backend/upload/views.py
+++ backend/upload/views.py
@@ -2,11 +2,19 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import FileUpload
 
 # Create your views here. Receieves the request and generates an HTTP page response
 
 def index(request):
-    return HttpResponse("<h1>This is the temp upload webpage</h1>")
+    all_uploads = FileUpload.objects.all()
+    html = ''
+    for upload in all_uploads:
+        url = '/upload/'+str(upload.id) + '/'
+        html += ' <a href= "' + url + '"> ' + upload.filename + '</a><br>'
+    return HttpResponse(html)
+
+   
 
 def detail(request, upload_id):
     return HttpResponse("<h2>Details for upload id: " + str(upload_id) + "</h2>")
